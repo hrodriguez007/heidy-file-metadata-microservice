e98799f4bed6ceab8e9e63ac9fb5a4bfc2a01195
hrodriguez007 <heidyrodriguez567@gmail.com>
2023-10-24 12:29:26 -0500

    Add multer dependency to package.json and require multer in index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+const upload = multer({ dest: 'uploads/' })
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// method="POST" action="/api/fileanalyse"
+app.post("/api/fileanalyse", (req, res) => {
 
-
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
