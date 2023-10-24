efd5c0c68d0e8d0c1b9618134e65417ca90070ab
hrodriguez007 <heidyrodriguez567@gmail.com>
2023-10-24 12:52:46 -0500

    Add app.post with the upload ability from multer and reassigned the output to be name, type, and size

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: './public/data/uploads' })
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// method="POST" action="/api/fileanalyse"
-app.post("/api/fileanalyse", (req, res) => {
-
+// name type size
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  });
 });
 
 const port = process.env.PORT || 3000;
